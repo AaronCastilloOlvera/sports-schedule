8fd667d9e9d189b6c242d8721060ed53e9570c03
Aarón Castillo <aaroncastilloolvera@gmail.com>
2025-02-05 21:29:13 -0600

    Add header and Remore CSS files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
-import './App.css'
-import { Box, Button, Typography  } from '@mui/material/Button';
+import { useEffect, useState } from 'react'
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    var myHeaders = new Headers();
+    myHeaders.append("x-rapidapi-key", "510d14a176b8062557cfa86f735abf14");
+    myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("https://v3.football.api-sports.io/leagues", requestOptions)
+      .then(response => response.json())
+      .then(result => setData(result))
+      .catch(error => console.log('error', error));
+  }, [])
+
+  const Header = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6">Sports Schedule</Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
   return (
-    <Box>
-      <Typography>Material-UI</Typography>
-      <Button variant="contained">Hello World</Button>
-    </Box>
+      <>
+        <Header />
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      </>
   )
 }
 
