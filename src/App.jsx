14efbe62b343cbd662033673a22146202d5fda65
Aarón Castillo <aaroncastilloolvera@gmail.com>
2025-12-17 00:31:46 -0600

    Remove unnnecesary useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import { useEffect, useState } from 'react'
 import { Box } from "@mui/material";
 import Header from './Header';
 import './App.css';
-import { apiClient } from './api';
 import Sidebar from './Sidebar';
 import Fixtures from './Fixtures';
 
 function App() {
-  const [leagues, setLeagues] = useState(null)
-  
-  useEffect(() => {
-    let mounted = true;
-    
-    apiClient.fetchFavoriteLeagues()
-      .then((leagues) => { if (mounted) setLeagues(leagues); })
-      .catch((error) => console.error(error));
-
-    return () => { mounted = false; };
-  }, [])
   return (
     
     <Box sx={{ display: 'flex' }}>
