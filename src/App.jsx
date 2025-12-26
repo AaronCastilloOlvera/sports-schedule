33d52936a385297586614e7c6da06c8adb9acf02
Aarón Castillo <aaroncastilloolvera@gmail.com>
2025-12-26 01:00:35 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
       <Header />
-      <Box sx={{ flexGrow: 1, p: 2 }}>
+      <Box sx={{ flexGrow: 1, p: 2, maxWidth: '1200px', margin: '0 auto', width: '100%' }}>
         <FutbolDashboard />
       </Box>
     </Box>
